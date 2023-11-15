33dc1519412f69cea368ab05e206f89604bdfe17
Nathan Chu <nathanchu05@gmail.com>
2023-11-15 16:54:06 -0500

    base code

--- app/src/main/java/com/example/androidfa23/MainActivity.kt
+++ app/src/main/java/com/example/androidfa23/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.androidfa23
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView : BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.browse -> {
+                    supportFragmentManager.beginTransaction().
+                    replace(R.id.fragmentContainerView, OrganizationsFragment.newInstance("", ""))
+                        .commit()
+                }
+                R.id.message -> {
+                    supportFragmentManager.beginTransaction().
+                    replace(R.id.fragmentContainerView, MessageFragment.newInstance("", ""))
+                        .commit()
+                }
+                R.id.calendar -> {
+                    supportFragmentManager.beginTransaction().
+                    replace(R.id.fragmentContainerView, CalendarFragment.newInstance("", ""))
+                        .commit()
+                }
+                R.id.profile -> {
+                    supportFragmentManager.beginTransaction().
+                    replace(R.id.fragmentContainerView, ProfileFragment.newInstance("", ""))
+                        .commit()
+                }
+            }
+            true
+        }
     }
 }
