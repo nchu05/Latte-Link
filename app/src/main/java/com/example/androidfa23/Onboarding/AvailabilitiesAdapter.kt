572b4de803a440fb046b3ad56af9f481090c7ef0
Nathan Chu <nathanchu05@gmail.com>
2023-11-29 11:59:25 -0500

    updates

--- app/src/main/java/com/example/androidfa23/Onboarding/AvailabilitiesAdapter.kt
+++ app/src/main/java/com/example/androidfa23/Onboarding/AvailabilitiesAdapter.kt
@@ -1,19 +1,24 @@
 package com.example.androidfa23
 
 
+import android.content.res.ColorStateList
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.example.androidfa23.Data.OrgAffiliations
 
 class AvailabilitiesAdapter (private var dataset: List<String>): RecyclerView.Adapter<AvailabilitiesAdapter.ViewHolder>()   {
     class ViewHolder(view: View): RecyclerView.ViewHolder(view){
         val time: Button
+        val X : TextView
         init{
             time = view.findViewById(R.id.time)
+            X = view.findViewById(R.id.X)
         }
     }
 
@@ -31,5 +36,13 @@ class AvailabilitiesAdapter (private var dataset: List<String>): RecyclerView.Ad
 
     override fun onBindViewHolder(holder: AvailabilitiesAdapter.ViewHolder, position: Int) {
         holder.time.text = dataset[position]
+        holder.X.setOnClickListener {
+            holder.X.isVisible = false
+            holder.time.backgroundTintList = ContextCompat.getColorStateList(holder.time.context, R.color.white)
+        }
+        holder.time.setOnClickListener {
+            holder.X.isVisible = true
+            holder.time.backgroundTintList = ColorStateList.valueOf(holder.time.context.resources.getColor(R.color.beige))
+        }
     }
 }
